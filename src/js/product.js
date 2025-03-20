a06c6687c5bfc0fcf7ece8e617a30b5515475904
hwcall1102 <haydenc0293@gmail.com>
2025-03-19 23:08:31 -0600

    product detail and cart pages

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData(category);
+const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
